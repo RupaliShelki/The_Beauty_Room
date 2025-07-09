107cd11b9cc377100997706e37dc9916ca403ad0
RupaliShelki <114135175+RupaliShelki@users.noreply.github.com>
2025-07-09 21:29:12 +0530

    components

---
+++ the-beauty-room/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import {Navbar} from  "./components/Navbar"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+     {/* <div className="text-center mt-10 text-3xl text-pink-600 font-bold">
+      Makeup Portfolio Started!
+    </div> */}
+    </>
+  )
+}
+
+export default App
