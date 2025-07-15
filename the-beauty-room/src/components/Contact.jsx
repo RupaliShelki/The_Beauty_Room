776b298ce55145199f6895eb3cb8eb1b129febfc
RupaliShelki <114135175+RupaliShelki@users.noreply.github.com>
2025-07-15 13:00:11 +0530

    form

--- the-beauty-room/src/components/Contact.jsx
+++ the-beauty-room/src/components/Contact.jsx
@@ -1,117 +1,89 @@
 import React from "react";
-// import { ChevronDownIcon } from '@heroicons/react/16/solid'
 
 function Contact() {
   return (
     <div className="w-full h-[100vh] bg-white flex gap-[6%] px-[6%] py-[4%]">
       <div className="w-[50%] bg-pink-300 p-[3%]">
-        <div>
-          <span>Get in Touch</span>
-          <form>
-            <div className="mt-10">
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="first-name"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                Name
-                </label>
-                <div className="">
-                  <input
-                    id="first-name"
-                    name="first-name"
-                    type="text"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
+        <span className="text-pink-950 font-bold text-2xl">Get in Touch</span>
+        <p className="text-pink-800">
+          For bookings or inquiries, please fill out the form below or reach out via WhatsApp.
+        </p>
 
-              <div className="sm:col-span-3 mt-2">
-                <label
-                  htmlFor="last-name"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Phone Number
-                </label>
-                <div className="">
-                  <input
-                    id="last-name"
-                    name="last-name"
-                    type="telephone"
-                    autoComplete="family-name"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
+        <form className="mt-10">
+          {/* Name */}
+          <label htmlFor="first-name" className="block text-sm font-medium text-gray-900">
+            Name
+          </label>
+          <input
+            id="first-name"
+            name="first-name"
+            type="text"
+            autoComplete="given-name"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900  outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-pink-600"
+          />
 
-              <div className="sm:col-span-4  mt-2">
-                <label
-                  htmlFor="email"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Email address
-                </label>
-                <div className="">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
+          {/* Phone Number */}
+          <label htmlFor="phone" className="block text-sm font-medium text-gray-900 mt-2">
+            Phone Number
+          </label>
+          <input
+            id="phone"
+            name="phone"
+            type="tel"
+            autoComplete="tel"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900  outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-pink-600"
+          />
 
-               <div className="sm:col-span-4  mt-2">
-                <label
-                  htmlFor="email"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Event Type
-                </label>
-                <select
-                  id="event"
-                  name="event"
-                  autoComplete="event-name"
-                  className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                >
-                  <option>Wedding/Reception</option>
-                  <option>Birthday</option>
-                  <option>Shoot</option>
-                  <option>Other</option>
-                </select>
-              </div>
-              <div className="sm:col-span-4  mt-2">
-                <label
-                  htmlFor="email"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Event Date
-                </label>
-                <div className="">
-                  <input
-                    id="date"
-                    name="date"
-                    type="date"
-                    autoComplete="email"
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
-            </div>
+          {/* Email */}
+          <label htmlFor="email" className="block text-sm font-medium text-gray-900 mt-2">
+            Email address
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="email"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900  outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-pink-600"
+          />
 
-            <div className="mt-6 ">
-              <button
-                type="submit"
-                className="rounded-md bg-pink-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-pink-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
+          {/* Event Type */}
+          <label htmlFor="event" className="block text-sm font-medium text-gray-900 mt-2">
+            Event Type
+          </label>
+          <select
+            id="event"
+            name="event"
+            autoComplete="event-name"
+            className="w-full appearance-none rounded-md bg-white py-1.5 px-3 text-base text-gray-900  outline-1 outline-gray-300 focus:outline-2 focus:outline-pink-600"
+          >
+            <option>Wedding/Reception</option>
+            <option>Birthday</option>
+            <option>Shoot</option>
+            <option>Other</option>
+          </select>
+
+          {/* Event Date */}
+          <label htmlFor="date" className="block text-sm font-medium text-gray-900 mt-2">
+            Event Date
+          </label>
+          <input
+            id="date"
+            name="date"
+            type="date"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900  outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-pink-600"
+          />
+
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="mt-6 rounded-md bg-pink-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-pink-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
+          >
+            Submit
+          </button>
+        </form>
       </div>
+
+      {/* Right Section */}
       <div className="w-[50%] bg-pink-300"></div>
     </div>
   );
